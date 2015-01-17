d6bb348578aa5679d4918e739b0e9888a6bf2dd7
Matt Del Signore <mattdelsig@me.com>
2015-01-17 14:01:03 -0500

    added code to make it a web server, sends data about the current pwm pin over the internet

--- pillbox/pillbox.ino
+++ pillbox/pillbox.ino
@@ -1,6 +1,12 @@
+#include <SPI.h>
+#include <Dhcp.h>
+#include <Dns.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
 #include <Time.h>
 
-#include <Time.h>
 
 /**
 Code for the hackathon IoT Pill box
@@ -8,7 +14,7 @@ By Nazim Amin and Matthew Del Signore
 **/
 
 int resistorValue; //the value we get from the photoresistor
-int currentLED; //the led that should be blinking for the current day
+int currentLED=0; //the led that should be blinking for the current day
 
 int SUNDAY = 0; //PWM slot for the LED for sunday
 int MONDAY = 1;
@@ -18,6 +24,12 @@ int THURSDAY = 4;
 int FRIDAY =  5;
 int SATURDAY = 6;
 
+char ssid[] = "yourNetwork";      // SSID (name) 
+char pass[] = "secretPassword";   // network password
+int keyIndex = 0;                 // network key Index number (needed only for WEP)
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
 void setup() {
   //start reading serial data
   Serial.begin(9600);
@@ -26,11 +38,86 @@ void setup() {
    for(int k=2;k<9;k++){ //turn off all the 
     pinMode(k,OUTPUT);
   }
+  
+   // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present"); 
+    while(true);        // don't continue
+  } 
+
+  String fv = WiFi.firmwareVersion();
+  if( fv != "1.1.0" )
+    Serial.println("Please upgrade the firmware");
+
+  // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) { 
+    Serial.print("Attempting to connect to Network named: ");
+    Serial.println(ssid);                   // print the network name (SSID);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    delay(10000);
+  } 
+  server.begin();                           // start the web server on port 80
+  printWifiStatus();                        // you're connected now, so print out the status
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
+  
+  //start the client 
+  WiFiClient client = server.available();
+  
+  if(client){ //if there is a client
+    Serial.println("new client connected");
+    
+    String clientMessage = "";
+    
+    while(client.connected()){
+      boolean newLine = true;
+      if(client.available()){ //if the client is sending data
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && newLine) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+
+          //output the current pwm pin that's in use
+          client.print("the current pwm is");
+          client.print(currentLED);
+          client.println("</br>");
+          
+          client.println("</html>");
+           break;
+        }
+        
+        if (c == '\n') {
+          // you're starting a new line
+          newLine = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          newLine = false;
+        }
+      }
+    }
+    
+    delay(1);
+    client.stop(); //disconnect the client
+    Serial.println("client disconnected");
+  }
+    
   
   /*resistorValue = analogRead(0); //read in analog input at point 0
   Serial.print(resistorValue);
@@ -54,4 +141,25 @@ void loop() {
   
   //wait 5 s
   delay(5000);
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
 }
