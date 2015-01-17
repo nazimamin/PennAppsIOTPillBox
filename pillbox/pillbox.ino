3b7f62c74a716cd15cf1f8d026d1d3e5aaea436d
Matt Del Signore <mattdelsig@me.com>
2015-01-17 09:14:30 -0500

    added code to get the current date

--- pillbox/pillbox.ino
+++ pillbox/pillbox.ino
@@ -1,5 +1,7 @@
 #include <Time.h>
 
+#include <Time.h>
+
 /**
 Code for the hackathon IoT Pill box
 By Nazim Amin and Matthew Del Signore
@@ -24,9 +26,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly: 
   
-  resistorValue = analogRead(0); //read in analog input at point 0
+  /*resistorValue = analogRead(0); //read in analog input at point 0
   Serial.print(resistorValue);
-  Serial.print(" "); 
+  Serial.print(" "); */
+  
+  Serial.print("the current date is");
+  Serial.print(day(now()));
+  Serial.print("\n");
   
   //wait 100ms
   delay(100);
