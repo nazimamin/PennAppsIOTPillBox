bab6dbec302c985ef94cd99cc5ee6e9dafbc9a01
Matt Del Signore <mattdelsig@me.com>
2015-01-17 04:39:21 -0500

    added the arduino code

---
+++ pillbox/pillbox.ino
@@ -0,0 +1,33 @@
+#include <Time.h>
+
+/**
+Code for the hackathon IoT Pill box
+By Nazim Amin and Matthew Del Signore
+**/
+
+int resistorValue; //the value we get from the photoresistor
+
+int SUNDAY = 0; //PWM slot for the LED for sunday
+int MONDAY = 1;
+int TUESDAY = 2;
+int WEDNESDAY = 3;
+int THURSDAY = 4;
+int FRIDAY =  5;
+int SATURDAY = 6;
+
+void setup() {
+  //start reading serial data
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+  resistorValue = analogRead(0); //read in analog input at point 0
+  Serial.print(resistorValue);
+  Serial.print(" "); 
+  
+  //wait 100ms
+  delay(100);
+}
